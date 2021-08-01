e76cfc00caf6a69590cefbdd366192455f7b3c0e
Glitch (glitch-hello-website) <none>
2021-08-01 20:33:10 +0000

    🍍📻 Checkpoint ./interaction.js:32136914/22764 ./.glitch-assets:32136914/2682

--- interaction.js
+++ interaction.js
@@ -1,13 +1,14 @@
 // Name any p5.js functions we use in `global` so Glitch can recognize them.
 /* global
  *    background, color, createCanvas, createSprite, drawSprites, fill,
- *    loadImage, loadAnimation, mouseIsPressed, mouseX, mouseY, random,
- *    text, textAlign, windowWidth, windowHeight, width, height
- *    UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, CENTER
+ *    keyIsDown, loadImage, loadAnimation, mouseIsPressed, mouseX, mouseY,
+ *    random, text, textAlign, windowWidth, windowHeight
+ *    UP_ARROW, DOWN_ARROW
  */
 
 const CDN = "https://cdn.glitch.com/eeb7d150-fc97-47a5-a1d3-a53d7d323bbe%2F";
 
+let velocity;
 let sprite;
 let floating;
 let moving;
@@ -15,6 +16,8 @@ let spinning;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  velocity = 2;
 
   floating = loadAnimation(
     `${CDN}ghost_standing0001.png`,
@@ -34,16 +37,12 @@ function setup() {
   spinning = loadAnimation(
     `${CDN}ghost_spin0001.png`,
     `${CDN}ghost_spin0002.png`,
-    `${CDN}ghost_spin0003.png`,
-    `${CDN}ghost_spin0004.png`
+    `${CDN}ghost_spin0003.png`
   );
 
-  sprite = createSprite(400, 200);
-
+  sprite = createSprite(400, (1 / 2) * windowHeight);
   sprite.addAnimation("floating", floating);
-
   sprite.addAnimation("moving", moving);
-
   sprite.addAnimation("spinning", spinning);
 }
 
@@ -52,33 +51,34 @@ function draw() {
 
   if (mouseX < sprite.position.x - 10) {
     sprite.changeAnimation("moving");
-    //flip horizontally
     sprite.mirrorX(-1);
-    //negative x velocity: move left
-    sprite.velocity.x = -2;
+    sprite.velocity.x = -velocity;
   } else if (mouseX > sprite.position.x + 10) {
     sprite.changeAnimation("moving");
-    //unflip
     sprite.mirrorX(1);
-    sprite.velocity.x = 2;
+    sprite.velocity.x = velocity;
   } else {
-    //if close to the mouse, don't move
     sprite.changeAnimation("floating");
     sprite.velocity.x = 0;
   }
 
   if (mouseIsPressed) {
-    //the rotation is not part of the spinning animation
     sprite.rotation -= 10;
     sprite.changeAnimation("spinning");
-  } else sprite.rotation = 0;
+  } else {
+    sprite.rotation = 0;
+  }
 
-  //up and down keys to change the scale
-  //note that scaling the image quality deteriorates
-  //and scaling to a negative value flips the image
-  if (keyIsDown(UP_ARROW)) sprite.scale += 0.05;
-  if (keyIsDown(DOWN_ARROW)) sprite.scale -= 0.05;
+  if (keyIsDown(UP_ARROW)) {
+    sprite.scale += 0.05;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    sprite.scale -= 0.05;
+  }
 
-  //draw the sprite
+  if (mouseIsPressed) {
+    sprite.visible = !sprite.visible;
+  }
+
   drawSprites();
 }

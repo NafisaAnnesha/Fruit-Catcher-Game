c9547932c38ca141e9a151602e5618b041cc95a5
Glitch (glitch-hello-website) <none>
2021-08-02 23:12:43 +0000

    🍬🎷 Checkpoint ./game.js:33828922/1116 ./index.html:33828922/72

---
+++ game.js
@@ -0,0 +1,19 @@
+// Name any p5.js functions we use in `global` so Glitch can recognize them.
+/* global
+ *    background, color, createCanvas, createSprite, drawSprites, loadImage,
+ *    loadAnimation, windowWidth, windowHeight
+ *    UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, CENTER, circle
+ */
+function setup() {
+  createCanvas(windowWidth, windowHeight);}
+function draw() {
+  background(255);
+
+
+
+
+ 
+}
+class Level{
+  
+}

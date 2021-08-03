d17cfe7fd6d4cba640aa6bd6711297ecda639ad0
Glitch (glitch-hello-website) <none>
2021-08-03 00:21:53 +0000

    🎫🌼 Checkpoint ./game.js:33135134/3943:33137364/500 ./.glitch-assets:33137364/384

--- game.js
+++ game.js
@@ -5,24 +5,36 @@
  *    createButton, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, CENTER, circle, random, width, height, noStroke, ellipse, fill
  */
 
-let watermelon;
+let watermelon, pear, orange, lemon, cherry, banana, apple;
 
 let fruits;
 let numFruit = 3;
 let score = 0;
+let basket;
+let img;
 
 function preLoad() {
   watermelon = loadImage("assets/Watermelon.png");
+  pear = loadImage("assets/Pear.png");
+  orange = loadImage("assets/Orange.png");
+  lemon = loadImage("assets/Lemon.png");
+  cherry = loadImage("assets/Cherry.png");
+  banana = loadImage("assets/Banana.png");
+  apple = loadImage("assets/Apple.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+
   // IMAGES
   // image(watermelon, 0, 0);
-  
+
+  // here we use a callback to display the image after loading. 
+  img = loadImage(
+    "https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2FfruitCollector.jpg?v=1627949883831");
+
   fruits = [];
-  
+
   for (let i = 0; i < numFruit; i++) {
     let newFruit = new Fruit();
     fruits.push(newFruit);
@@ -30,25 +42,25 @@ function setup() {
 }
 
 class Fruit {
-  constructor () {
+  constructor() {
     this.x = random(width);
     this.y = 0;
     this.radius = random(20, 30);
     this.fallSpeed = random(0.5, 1);
   }
-  
+
   move() {
     this.y += this.fallSpeed;
-    
+
     if (this.y > height) {
       this.y = 0;
       this.x = random(width);
     }
   }
-  
+
   display() {
     noStroke();
-    fill('red') // PLACEHOLDER, replace with image
+    fill("red"); // PLACEHOLDER, replace with image
     ellipse(this.x, this.y, this.radius);
   }
 } // end of Fruit
@@ -59,20 +71,22 @@ function draw() {
   for (let i = 0; i < fruits.length; i++) {
     let fruit = fruits[i];
     fruit.move();
-    fruit.display()
+    fruit.display();
   }
+  
+  // fruitCollector image. 
+  image(img, 260, 260);
 }
 
-class Level{
-  constructor(button, bg){
+class Level {
+  constructor(button, bg) {
     this.button = button;
     this.background = bg;
   }
-  
-  
 }
-let bg = loadImage("https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2Fbg.png?v=1627946598416")
-let button = createButton('click me')
-  button.position(0, 0);
-let welcomeScreen = new Level(bg, button)
-
+let bg = loadImage(
+  "https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2Fbg.png?v=1627946598416"
+);
+let button = createButton("click me");
+button.position(0, 0);
+let welcomeScreen = new Level(bg, button);

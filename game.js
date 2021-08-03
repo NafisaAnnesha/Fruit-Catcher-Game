b94a3592981ccd2fc09f820d9b85ead1720d2971
Glitch (glitch-hello-website) <none>
2021-08-03 01:49:50 +0000

    ⚓️🌖 Checkpoint ./game.js:33135134/7837

--- game.js
+++ game.js
@@ -6,7 +6,7 @@
  */
 
 let watermelon, pear, orange, lemon, cherry, banana, apple;
-
+let scale = 7;
 let fruits;
 let numFruit = 3;
 let score = 0;
@@ -14,25 +14,36 @@ let basket;
 let img;
 
 function preLoad() {
-loadImage("assets/Watermelon.png", watermelon => image(watermelon, 0, 0));
-// loadImage("assets/Pear.png", => {image(,0, 0)});
-// loadImage("assets/Orange.png", => {image(,0, 0)});
-// loadImage("assets/Lemon.png", => {image(,0, 0)});
-// loadImage("assets/Cherry.png", => {image(,0, 0)});
-// loadImage("assets/Banana.png", => {image(,0, 0)});
-// loadImage("assets/Apple.png", => {image(,0, 0)});
+  watermelon = loadImage(
+    "https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2FWatermelon.png?v=1627948820847"
+  );
+  pear = loadImage(
+    "https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2FPear.png?v=1627948814175"
+  );
+  orange = loadImage(
+    "https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2FOrange.png?v=1627948802920"
+  );
+  lemon = loadImage(
+    "https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2FLemon.png?v=1627948795527"
+  );
+  cherry = loadImage(
+    "https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2FCherry.png?v=1627948787922"
+  );
+  banana = loadImage(
+    "https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2FBanana.png?v=1627948780734"
+  );
+  apple = loadImage(
+    "https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2FApple.png?v=1627948772526"
+  );
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // IMAGES
-  
-
-
-  // here we use a callback to display the image after loading. 
+  // here we use a callback to display the image after loading.
   img = loadImage(
-    "https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2FfruitCollector.jpg?v=1627949883831");
+    "https://cdn.glitch.com/d8cd1a49-283f-47bb-acc5-1f438d6c1b79%2FfruitCollector.jpg?v=1627949883831"
+  );
 
   fruits = [];
 
@@ -73,12 +84,19 @@ function draw() {
     let fruit = fruits[i];
     fruit.move();
     fruit.display();
-    
-
   }
-  
-  // fruitCollector image. 
+
+  // fruitCollector image.
   image(img, 320, 390, 100, 100);
+
+  // Images
+  image(watermelon, 0, 0, 1150 / (scale + 4), 475 / (scale + 4));
+  image(pear, 0, 0, 239 / scale, 359 / scale);
+  image(orange, 0, 0, 239 / scale, 237 / scale);
+  image(lemon, 250, 0, 212 / scale, 286 / scale);
+  image(cherry, 150, 0, 686 / scale, 444 / scale);
+  image(banana, 140, 0, 327 / scale, 420 / scale);
+  image(apple, 70, 0, 239 / scale, 270 / scale);
 }
 
 class Level {

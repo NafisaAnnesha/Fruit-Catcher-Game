97f70ccf50a2cb3167b78240e501804f41ca8067
Glitch (glitch-hello-website) <none>
2021-08-06 06:12:42 +0000

    🍿🤑 Checkpoint ./game.js:33828922/1028

--- game.js
+++ game.js
@@ -32,11 +32,12 @@ let character, characterX, characterY, characterZ;
 let isJumping, jumpHeight, jumpDirection;
 
 let bg1, bg2, bg3, bg4;
-let button1, button2, button3, button4;
+let button1, button2, button3, button4, button5;
 let bgImg1, bgImg2, bgImg3, bgImg4;
 
 let pressed;
 let pressed2;
+let pressed3;
 let nextLevel;
 let restart;
 let fallSpeed;
@@ -88,6 +89,7 @@ function setup() {
   nextLevel = false;
   pressed = false;
   pressed2 = false;
+  pressed3 = false;
   restart = false;
   restart = false;
   gameIsOver = false;
@@ -269,13 +271,20 @@ function draw() {
     gameOverSound.stop();
   }
   checkLost();
-
+ 
   button1.mousePressed(() => (pressed = true));
   button4.mousePressed(() => (restart = true));
   button2.mousePressed(() => (pressed2 = true));
+  
 
   if (pressed2) {
     instruction.instructionDisplay();
+    //console.log("hey")
+     
+  }
+  
+  if (pressed3) {
+     welcomeScreen.display1();
     //console.log("hey")
      
   }
@@ -350,7 +359,7 @@ class Level {
     textFont(fontBold);
     text("Catch The Fruit", (1 / 3) * width, (1 / 2) * height);
     button1.position((1 / 2) * width, 0.7 * height);
-    button2.position((1 / 3) * height, (1 / 4) * height);
+    button2.position((.1) * width, 0.1* height);
     button1.size(150, 50);
     button1.style("background-color", btnCol);
   }
@@ -422,14 +431,15 @@ class Level {
 
   instructionDisplay() {
     
-   
+   button5 = createButton("back")
+    button5.position(width-1400, height-100)
     textSize(30)
     fill(36, 42, 43)
-    text("Collect the fruits to earn points" , width-1400, height-400 )
+    text("Collect the fruits to earn points" , width-1400, height-600 )
     text("Move the mouse to collect fruits" , width-1400, height-500 )
-    text("Catch the fruits by jumping to earn double point" , width-1400, height-600 )
-    text("Catching the rotten fruit will deduct score by 1" , width-1400, height-700 )
-    text("failing to catch 5 fruits will result in Game Over" , width-1400, height-800 )
+    text("Catch the fruits by jumping to earn double point" , width-1400, height-400 )
+    text("Catching the rotten fruit will deduct score by 1" , width-1400, height-300 )
+    text("failing to catch 5 fruits will result in Game Over" , width-1400, height-200 )
        
   }
 }
